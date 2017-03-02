adca61ca263fe019c34b3bee55cac34a5e50e73e
Dmitry Kryukov <remasik@gmail.com>
2017-03-02 13:27:39 -0500

    Added radar chart example

--- public/js/commits_per_user.js
+++ public/js/commits_per_user.js
@@ -7,6 +7,7 @@ var ctx_doughnut = document.getElementById("commits_per_user_doughnut");
 
 line_chart();
 bar_chart();
+radar_chart();
 
 function line_chart(){
 
@@ -85,4 +86,39 @@ function bar_chart(){
         }
     });
 
+};
+
+function radar_chart(){
+
+    var data = {
+        labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+        datasets: [
+            {
+                label: "My First dataset",
+                backgroundColor: "rgba(179,181,198,0.2)",
+                borderColor: "rgba(179,181,198,1)",
+                pointBackgroundColor: "rgba(179,181,198,1)",
+                pointBorderColor: "#fff",
+                pointHoverBackgroundColor: "#fff",
+                pointHoverBorderColor: "rgba(179,181,198,1)",
+                data: [65, 59, 90, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                backgroundColor: "rgba(255,99,132,0.2)",
+                borderColor: "rgba(255,99,132,1)",
+                pointBackgroundColor: "rgba(255,99,132,1)",
+                pointBorderColor: "#fff",
+                pointHoverBackgroundColor: "#fff",
+                pointHoverBorderColor: "rgba(255,99,132,1)",
+                data: [28, 48, 40, 19, 96, 27, 100]
+            }
+        ]
+    };
+    var options = {};
+    var chart = new Chart(ctx_radar, {
+        type: 'radar',
+        data: data,
+        options: options
+    });
 };
