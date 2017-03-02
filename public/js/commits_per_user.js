fba4031bf04d528ae7ae354438c759fa2dc8f074
Dmitry Kryukov <remasik@gmail.com>
2017-03-02 13:27:02 -0500

    Added bar chart example

--- public/js/commits_per_user.js
+++ public/js/commits_per_user.js
@@ -6,6 +6,7 @@ var ctx_pie = document.getElementById("commits_per_user_pie");
 var ctx_doughnut = document.getElementById("commits_per_user_doughnut");
 
 line_chart();
+bar_chart();
 
 function line_chart(){
 
@@ -42,6 +43,46 @@ function line_chart(){
         type: 'line',
         data: data,
         options: options
+    });
+
+};
+
+function bar_chart(){
+    var chart = new Chart(ctx_bar, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
     });
 
 };
