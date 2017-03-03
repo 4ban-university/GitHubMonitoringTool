d8b116badbb8b7665422298c64a91dc644d93b35
Dmitry Kryukov <remasik@gmail.com>
2017-03-03 08:37:26 -0500

    Deleted unused files

--- public/js/comments_per_user.js
+++
@@ -1,6 +0,0 @@
-var ctx_line = document.getElementById("comments_per_user_line");
-var ctx_bar = document.getElementById("comments_per_user_bar");
-var ctx_radar = document.getElementById("comments_per_user_radar");
-var ctx_polar = document.getElementById("comments_per_user_polar");
-var ctx_pie = document.getElementById("comments_per_user_pie");
-var ctx_doughnut = document.getElementById("comments_per_user_doughnut");
