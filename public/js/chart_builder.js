44c2228fde18b70c276a105fd774ce0d1228dee5
Dmitry Kryukov <remasik@gmail.com>
2017-03-05 20:57:16 -0500

    Added burndown tab functions

--- public/js/chart_builder.js
+++ public/js/chart_builder.js
@@ -50,6 +50,118 @@ function overAllInfo(){
 	
 };
 
+function burndown(){
+	var ctx_burndown = document.getElementById('burndown');	
+	var burndown_data = repo.burndown;
+	var burndown_options = {
+			chartArea: {
+        				backgroundColor: 'rgba(255, 255, 255, 1)'
+    		}
+    	};
+
+	
+	var keyNum=0
+	var labels=[]
+
+	for (var key in burndown_data) {
+		labels[keyNum]='Day '+key
+		keyNum++
+	}
+
+	var keyNum=0
+	var data=[]
+	for (var key in burndown_data) {
+		data[keyNum]=burndown_data[key]
+		keyNum++
+	}
+
+	var i = 0;
+	var ideal_data = [];
+	ideal_data[0]=data[0]
+	var firstIssues = data[0];
+	var totalDays = labels.length;
+	var idealIncrement = firstIssues/(totalDays-1)
+	for (var i = 1; i < labels.length; i++){
+		firstIssues-=idealIncrement;
+		ideal_data[i]=firstIssues
+
+		}
+	ideal_data[ideal_data.length-1] = 0
+
+	burndown_data = {
+		labels: labels,
+
+		datasets: [
+        {
+            data: data,
+            label: " Burndown chart",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 5,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(75,192,192,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            spanGaps: false,
+        },
+        {	
+        	data: ideal_data,
+        	label: " Ideal burndown chart",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(114,208,114,0.4)",
+            borderColor: "rgba(114,208,114,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(114,208,114,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 5,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(114,208,114,1)",
+            pointHoverBorderColor: "rgba(114,208,114,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            spanGaps: false,
+        }]
+        
+
+	}
+
+
+	var burndown_chart = new Chart(ctx_burndown, {
+        type: 'line',
+        data: burndown_data,
+        options: burndown_options
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var table = "<table><tr><th>Name</th><th>Number of commits</th><th>Percentage</th></tr>"
 var commitsPerCollaborator=repo.commitsPerCollaborator
 for (var key in commitsPerCollaborator) {
@@ -75,7 +187,7 @@ table+="</table>"
 document.getElementById('commentsTable').innerHTML += table;
 
 charts(repo);
-burndown(repo);
+
 
  //----------checkboxes for what information to show
  document.getElementById('commitsPerCollaboratorCheck').onclick = function() {
@@ -271,116 +383,3 @@ function issuesPerCollaboratorTransformation (issuesPerCollaborator_data){
 
 };
 
-function burndown(repo){
-	var ctx_burndown = document.getElementById('burndown');	
-	var burndown_data = repo.burndown;
-	var burndown_options = {
-			chartArea: {
-        				backgroundColor: 'rgba(35, 35, 35, 0.6)'
-    		}
-    	};
-
-	
-	var keyNum=0
-	var labels=[]
-
-	for (var key in burndown_data) {
-		labels[keyNum]='Day '+key
-		keyNum++
-	}
-
-	var keyNum=0
-	var data=[]
-	for (var key in burndown_data) {
-		data[keyNum]=burndown_data[key]
-		keyNum++
-	}
-
-	var i = 0;
-	var ideal_data = [];
-	ideal_data[0]=data[0]
-	var firstIssues = data[0];
-	var totalDays = labels.length;
-	var idealIncrement = firstIssues/(totalDays-1)
-	for (var i = 1; i < labels.length; i++){
-		firstIssues-=idealIncrement;
-		ideal_data[i]=firstIssues
-
-		}
-	ideal_data[ideal_data.length-1] = 0
-	console.log(ideal_data)
-
-	burndown_data = {
-		labels: labels,
-
-		datasets: [
-        {
-            data: data,
-            label: " Burndown chart",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 5,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(75,192,192,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            spanGaps: false,
-        },
-        {	
-        	data: ideal_data,
-        	label: " Ideal burndown chart",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(114,208,114,0.4)",
-            borderColor: "rgba(114,208,114,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(114,208,114,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 5,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(114,208,114,1)",
-            pointHoverBorderColor: "rgba(114,208,114,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            spanGaps: false,
-        }]
-        
-
-	}
-
-
-	var burndown_chart = new Chart(ctx_burndown, {
-        type: 'line',
-        data: burndown_data,
-        options: burndown_options
-    });
-};
-
-// Chart js plugon for changing background color in charts. 
-Chart.pluginService.register({
-    beforeDraw: function (chart, easing) {
-        if (chart.config.options.chartArea && chart.config.options.chartArea.backgroundColor) {
-            var helpers = Chart.helpers;
-            var ctx = chart.chart.ctx;
-            var chartArea = chart.chartArea;
-            ctx.save();
-            ctx.fillStyle = chart.config.options.chartArea.backgroundColor;
-            ctx.fillRect(chartArea.left, chartArea.top, chartArea.right - chartArea.left, chartArea.bottom - chartArea.top);
-            ctx.restore();
-        }
-    }
-});
