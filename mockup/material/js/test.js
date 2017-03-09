6406d846aac55a4172ce613849045a0caab0491c
Aman Bhandal <aman_bhandal@live.ca>
2017-03-09 01:51:40 -0500

    Added comments

--- mockup/material/js/test.js
+++ mockup/material/js/test.js
@@ -3,6 +3,10 @@ $( document ).ready(function() {
 });
 
 
+/*
+    These are a collection of methods that display various types of modals
+    ########################################################################
+*/
 $('#show-info').click(function () {
     showDialog({
         title: 'Information',
@@ -33,7 +37,11 @@ $('#show-loading').click(function () {
         hideLoading();
     }, 3000);
 });
+//  ########################################################################
 
+/*
+    Retrives the list of repos associated with the user using the Github API
+*/
 function getRepo() {
 	
 	$.when( $.ajax("https://api.github.com/users/abhandal/repos")).done(function(data) {
@@ -62,6 +70,9 @@ function getRepo() {
 	});
 }
 
+/*
+    Sends an array to the writeToJSON.php file which writes the array to a json file
+*/
 function writeToJSON(repo_array) {
     $.ajax({
         url: 'resources/writeToJSON.php',
@@ -70,6 +81,9 @@ function writeToJSON(repo_array) {
     });
 }
 
+/*
+    Formats the output of text in the modal when the user clicks "+ Repo"
+*/
 function checkbox_list(repo_name, checkboxId){
    
     var h = "";
@@ -82,7 +96,31 @@ function checkbox_list(repo_name, checkboxId){
     return h;
 }
 
-/* library */
+
+/*
+    Retrives the list of repo names from the JSON file stored in resources by parsing it and appending the repo names to the index.html file
+*/
+function getRepoList() {
+    $.getJSON("resources/userRepoSelection.json", function(data) {
+        $('#repoSelection').html('');
+        for(var i = 0; i < data["repoSelection"].length; i++) {
+            $('#repoSelection').append('<a class="mdl-navigation__link" href="#" id="' +data["repoSelection"][i]["name"]+'"><i class="fa fa-github fa-2x" aria-hidden="true" role="presentation" style="margin-right: 10px;"></i>' +data["repoSelection"][i]["name"]+ '</a>')
+        }
+    });
+}
+
+/*
+    Tracks changes in the checkbox when the add repo modal is open and pushes the changes to the JSON file
+*/
+$('#repoSelection').change(function() {
+    console.log("i am here");
+    //console.log($( this ).prop("checked").val());
+});
+
+
+/* 
+    Modal Library -- Do not modify!
+*/
 function showLoading() {
     // remove existing loaders
     $('.loading-container').remove();
@@ -190,17 +228,3 @@ function hideDialog(dialog) {
         dialog.remove();
     }, 400);
 }
-
-function getRepoList() {
-    $.getJSON("resources/userRepoSelection.json", function(data) {
-        $('#repoSelection').html('');
-        for(var i = 0; i < data["repoSelection"].length; i++) {
-            $('#repoSelection').append('<a class="mdl-navigation__link" href="#" id="' +data["repoSelection"][i]["name"]+'"><i class="fa fa-github fa-2x" aria-hidden="true" role="presentation" style="margin-right: 10px;"></i>' +data["repoSelection"][i]["name"]+ '</a>')
-        }
-    });
-}
-
-$('#repoSelection').change(function() {
-    console.log("i am here");
-    //console.log($( this ).prop("checked").val());
-});
