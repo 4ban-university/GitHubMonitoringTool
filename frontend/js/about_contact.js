f4ef52b8d0d224e25303055001525c2641b4a64a
Raymart De Guzman <raymart54@gmail.com>
2017-03-25 18:44:47 -0400

    fix commit number. now its more boxy

--- frontend/js/about_contact.js
+++ frontend/js/about_contact.js
@@ -3,13 +3,13 @@ var about_button = document.querySelector('#about-btn-dialog'),
 
 var about_dialog = document.querySelector('#about-dialog'),
     contact_dialog = document.querySelector('#contact-dialog');
-about_button.addEventListener('click', function() {
+about_button.addEventListener('click', function () {
     about_dialog.showModal();
 });
 about_dialog.querySelector(".close").addEventListener('click', function () {
     about_dialog.close();
 });
-contact_button.addEventListener('click', function() {
+contact_button.addEventListener('click', function () {
     contact_dialog.showModal();
 });
 contact_dialog.querySelector(".close").addEventListener('click', function () {
