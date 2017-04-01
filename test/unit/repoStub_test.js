f209299e76523d4b58f99f505c4a10d54bda45de
Dmitry Kryukov <remasik@gmail.com>
2017-04-01 00:40:09 -0400

    Try to test repoStub

---
+++ test/unit/repoStub_test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+var jsdom = require("jsdom").jsdom;
+var doc = jsdom();
+var window = doc.defaultView;
+
+$ = require('jquery')(window);
+
+var r = require("./repoStub.js")
+
+var repo = new r.Repo();
+
+console.log(repo.link)
