e79a618cc305e0e4eb47aa7ac38fffbf6daaf4c0
Dmitry Kryukov <remasik@gmail.com>
2017-04-13 11:21:02 -0400

    Temporary comment test

--- test/unit/repoStub_test.js
+++ test/unit/repoStub_test.js
@@ -1,13 +1,13 @@
-var assert = require('assert');
+// var assert = require('assert');
 
-var jsdom = require("jsdom").jsdom;
-var doc = jsdom();
-var window = doc.defaultView;
+// var jsdom = require("jsdom").jsdom;
+// var doc = jsdom();
+// var window = doc.defaultView;
 
-$ = require('jquery')(window);
+// $ = require('jquery')(window);
 
-var r = require("./repoStub.js")
+// var r = require("./repoStub.js")
 
-var repo = new r.Repo();
+// var repo = new r.Repo();
 
-console.log(repo.link)
+// console.log(repo.link)
